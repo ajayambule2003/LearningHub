812bb04ce194a1652fba7c954a311b24e9804adb
Ajay Ambule <ajayambule2003@gmail.com>
2024-11-17 17:59:03 +0530

    READ file changed

--- read.php
+++ read.php
@@ -4,7 +4,7 @@ include('session.php');
 if (isset($_POST['read'])){
 $id=$_POST['selector'];
 $N = count($id);
-for($i=0; $i < $N; $i++)
+for($i=0; $i < $N; $i++) 
 {
 
 	mysqli_query($conn,"insert into notification_read (student_id,student_read,notification_id) values('$session_id','yes','$id[$i]')")or die(mysqli_error());
